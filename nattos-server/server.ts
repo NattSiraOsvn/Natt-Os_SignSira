27b15eda2c7498363c6caa279663cd1e12e1e30d
Băng <bang@natt-os.dev>
2026-03-29 14:20:19 +0700

    feat(server): serve nattos-ui tại /nauion

--- nattos-server/server.ts
+++ nattos-server/server.ts
@@ -21,6 +21,7 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../src/ui-app')));
+app.use('/nauion', express.static(path.join(__dirname, 'nattos-ui')));
 
 bootKernel();
 NauionVoice.wake();
